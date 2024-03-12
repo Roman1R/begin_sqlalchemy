71e32d9dc0618e88f7126331d8f4b08a8a4c9f15
HP <635roman.r642@gmail.com>
2024-03-12 17:08:12 +1000

    commit for task 3

---
+++ task3/data/users.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+import sqlalchemy.orm as orm
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    age = sqlalchemy.Column(sqlalchemy.Integer)
+    position = sqlalchemy.Column(sqlalchemy.String)
+    speciality = sqlalchemy.Column(sqlalchemy.String)
+    address = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime)
+    jobs = orm.relationship("Job", back_populates='user')
+
+    def __repr__(self):
+        return (self.id, self.surname, self.name, self.age, self.position, self.speciality, self.address, self.email,
+                self.hashed_password)
---
+++ task3/main.py
@@ -0,0 +1,74 @@
+from data import db_session
+from data.users import User
+
+"""
+    surname Scott
+    name Ridley
+    age 21
+    position captain
+    speciality research engineer
+    address module_1
+    email scott_chief@mars.org
+"""
+
+
+def main():
+    db_session.global_init("db/for_example.db")
+    db_sess = db_session.create_session()
+
+    # db_sess.query(User).delete()  # FUNCTION TO DELETE
+
+    cap = User()
+    cap.surname = "Scott"
+    cap.name = "Ridley"
+    cap.age = 21
+    cap.position = "captain"
+    cap.speciality = "research engineer"
+    cap.address = "module_2"
+    cap.email = "scott_chief@mars.org"
+
+    boatswain1 = User()
+    boatswain1.surname = "Mr.Smith"
+    boatswain1.name = "Tom"
+    boatswain1.age = 25
+    boatswain1.position = "member"
+    boatswain1.speciality = "boatswain"
+    boatswain1.address = "module_2"
+    boatswain1.email = "mr_tom@mars.org"
+
+    boatswain2 = User()
+    boatswain2.surname = "Absolutely_Hz"
+    boatswain2.name = "Hz"
+    boatswain2.age = 27
+    boatswain2.position = "member"
+    boatswain2.speciality = "boatswain"
+    boatswain2.address = "module_2"
+    boatswain2.email = "hz_hz_hz@mars.org"
+
+    cooker = User()
+    cooker.surname = "Mrs.Cook"
+    cooker.name = "Sussan"
+    cooker.age = 27
+    cooker.position = "spec_member"
+    cooker.speciality = "cooker"
+    cooker.address = "module_1"
+    cooker.email = "recipe_for_life@mars.org"
+
+    db_sess.add(cap)
+    db_sess.add(boatswain1)
+    db_sess.add(boatswain2)
+    db_sess.add(cooker)
+
+    db_sess.commit()
+
+    db_sess.delete(cap)
+    db_sess.delete(boatswain1)
+    db_sess.delete(boatswain2)
+    db_sess.delete(cooker)
+
+    db_sess.commit()
+    # app.run()
+
+
+if __name__ == '__main__':
+    main()
